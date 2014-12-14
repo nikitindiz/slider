033f4406a8e1f6f2d0bd8cda1eb8c066a0a04fca
Alexander Nikitin <nikitindiz@gmail.com>
2014-12-14 13:56:08 +0700

    Added to main html

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   function log_debug(debug_data)
   {
-    $('div.debug').append('<p>'+debug_data+'</p>');
+    // $('div.debug').append('<p>'+debug_data+'</p>');
   }
 
   // Detect browser
